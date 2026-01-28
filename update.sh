4b82ffb1441921a5be3c6ef9c3b18532b449e6ae
exception7601 <77030548+exception7601@users.noreply.github.com>
2026-01-28 15:24:27 -0300

    Add automated SwiftLint update workflow

--- update.sh
+++ update.sh
@@ -1,28 +1,37 @@
+#!/bin/sh
+
 set -e
 
-BUILD=$(date +%s)
 REPO=realm/SwiftLint
 NEW_NAME=SwiftLintBinary.artifactbundle.zip
-VERSION=$(gh release list \
-  --repo ${REPO} \
-  --exclude-pre-releases \
-  --limit 1 \
-  --json tagName -q '.[0].tagName'
+VERSION=$(
+  gh release list \
+    --repo ${REPO} \
+    --exclude-pre-releases \
+    --limit 1 \
+    --json tagName -q '.[0].tagName'
 )
 
-echo ${VERSION}
+# Check if the version already exists as a tag
+if git rev-parse "v${VERSION}" >/dev/null 2>&1; then
+  echo "Version ${VERSION} already exists. No update needed."
+  exit 0
+fi
+
+echo "${VERSION}"
 
 ASSET_URL=$(gh api \
-  repos/${REPO}/releases/tags/${VERSION} \
+  repos/${REPO}/releases/tags/"${VERSION}" \
   --jq '.assets[] | select(.name=="'"${NEW_NAME}"'") | .browser_download_url')
 
 echo "${ASSET_URL}"
 curl -L -o "${NEW_NAME}" "${ASSET_URL}"
 
-SUM=$(swift package compute-checksum ${NEW_NAME} )
+SUM=$(sha256sum ${NEW_NAME} | awk '{print $1}')
 DOWNLOAD_URL="${ASSET_URL}"
 
-PACKAGE=$(cat <<END
+PACKAGE=$(
+  cat <<END
 // swift-tools-version:5.9
 import PackageDescription
 
@@ -60,7 +69,7 @@ let package = Package(
 END
 )
 
-echo "$PACKAGE" > Package.swift
+echo "$PACKAGE" >Package.swift
 git add Package.swift
 git commit -m "new Version ${VERSION}"
 git tag -s -a "v${VERSION}" -m "v${VERSION}"
@@ -72,7 +81,8 @@ git push origin HEAD --tags
 # git commit -m "new Version ${VERSION}"
 # git checkout -b release-v${VERSION}
 
-NOTES=$(cat <<END
+NOTES=$(
+  cat <<END
 SPM Plugin
 \`\`\`
 dependencies: [
